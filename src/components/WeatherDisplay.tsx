c9497b400bf355d5c7533dddcc21fc60dbfea164
vansh7574 <vanshrodge107@gmail.com>
2023-10-01 12:00:31 -0500

    Changes in CSS

--- src/components/WeatherDisplay.tsx
+++ src/components/WeatherDisplay.tsx
@@ -1,6 +1,6 @@
-import { Paper, Typography } from "@mui/material";
+import { Paper, Typography, Box } from "@mui/material";
 import React from "react";
-import "./WD.css";
+
 //{(weatherData.main.temp - 273.15)*9/5 + 32} °F, {(weatherData.main.temp - 273.15)} °C
 export interface WeatherData {
   coord: {
@@ -54,37 +54,42 @@ const WeatherDisplay: React.FC<WeatherDisplayProps> = ({ weatherData }) => {
   }
   return (
     <Paper elevation={3} sx={{ p: 2, margin: "0 auto" }}>
-      <Typography variant="h6" gutterBottom>
-        Weather in {weatherData.name}, {weatherData.sys.country}
-      </Typography>
-      <Typography>
-        Main: {weatherData.weather[0].main},{" "}
-        {weatherData.weather[0].description}
-      </Typography>
-      <Typography>
-        Temperature: {weatherData.main.temp} °K /{"    "}
-        {(((weatherData.main.temp - 273.15) * 9) / 5 + 32).toFixed(2)} °F /
-        {"    "} {(weatherData.main.temp - 273.15).toFixed(2)} °C
-      </Typography>
-      <Typography>
-        Feels Like: {weatherData.main.feels_like} °K /{"    "}
-        {(((weatherData.main.feels_like - 273.15) * 9) / 5 + 32).toFixed(2)} °F
-        / {"    "}
-        {(weatherData.main.feels_like - 273.15).toFixed(2)} °C
-      </Typography>
-      <Typography>Min Temperature: {weatherData.main.temp_min} °K</Typography>
-      <Typography>Max Temperature: {weatherData.main.temp_max} °K</Typography>
-      <Typography>Pressure: {weatherData.main.pressure} hPa</Typography>
-      <Typography>Humidity: {weatherData.main.humidity}%</Typography>
-      <Typography>Wind Speed: {weatherData.wind.speed} m/s</Typography>
-      <Typography>Wind Direction: {weatherData.wind.deg}°</Typography>
-      <Typography>Cloudiness: {weatherData.clouds.all}%</Typography>
-      <Typography>
-        Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}
-      </Typography>
-      <Typography>
-        Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
-      </Typography>
+      <Box display="flex" flexDirection="column" gap={1}>
+        <Typography variant="h6" gutterBottom>
+          Weather in {weatherData.name}, {weatherData.sys.country}
+        </Typography>
+        <Typography>
+          Main: {weatherData.weather[0].main},{" "}
+          {weatherData.weather[0].description}
+        </Typography>
+        <Typography>
+          Temperature: {weatherData.main.temp} °K /{"    "}
+          {(((weatherData.main.temp - 273.15) * 9) / 5 + 32).toFixed(2)} °F /
+          {"    "} {(weatherData.main.temp - 273.15).toFixed(2)} °C
+        </Typography>
+        <Typography>
+          Feels Like: {weatherData.main.feels_like} °K /{"    "}
+          {(((weatherData.main.feels_like - 273.15) * 9) / 5 + 32).toFixed(
+            2
+          )}{" "}
+          °F / {"    "}
+          {(weatherData.main.feels_like - 273.15).toFixed(2)} °C
+        </Typography>
+        <Typography>Min Temperature: {weatherData.main.temp_min} °K</Typography>
+        <Typography>Max Temperature: {weatherData.main.temp_max} °K</Typography>
+        <Typography>Pressure: {weatherData.main.pressure} hPa</Typography>
+        <Typography>Humidity: {weatherData.main.humidity}%</Typography>
+        <Typography>Wind Speed: {weatherData.wind.speed} m/s</Typography>
+        <Typography>Wind Direction: {weatherData.wind.deg}°</Typography>
+        <Typography>Cloudiness: {weatherData.clouds.all}%</Typography>
+        <Typography>
+          Sunrise:{" "}
+          {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString()}
+        </Typography>
+        <Typography>
+          Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString()}
+        </Typography>
+      </Box>
     </Paper>
   );
 };
